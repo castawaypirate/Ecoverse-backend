cf34389440b9d9a7e0170e95479548184817f770
Ioannis Maliouris <maliouri@csd.auth.gr>
2020-05-23 01:54:34 +0300

    Initialize team model

---
+++ app/Team.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int         $id
+ * @property string      $name
+ * @property string      $description
+ * @property boolean     $public
+ * @property string      $created_at
+ * @property string      $updated_at
+ */
+class Team extends Model
+{
+}
