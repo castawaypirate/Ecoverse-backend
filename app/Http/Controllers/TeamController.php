3210a420b25c81b160cf89751bfc20796d2a3f3f
Ioannis Maliouris <maliouri@csd.auth.gr>
2020-06-14 17:53:53 +0300

    Update team controller

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,15 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Team;
+use App\TeamMember;
+use Auth;
+use Illuminate\Database\Eloquent\Builder;
 
 class TeamController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function read() {
+        $user = Auth::user();
+        $teams = Team::whereHas('members', function (Builder $q) use ($user) {
+            $q->where('users.id', $user->id);
+        });
+
+        return response()->json($teams->get());
+    }
+
     /**
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function readOne($id) {
+    public function readOne($team_id) {
+        $team = Team::where('id', $team_id);
 
+        if ($team->public) {
+            $team = $team->with('posts');
+        } else {
+            $user = Auth::user();
+            $is_member = $team->whereHas('members', function (Builder $q) use ($user) {
+                $q->where('users.id', $user->id);
+            });
+            if ($is_member) {
+                $team = $team->with('posts');
+            }
+        }
+
+        return response()->json($team->get());
     }
 
     /**
@@ -25,19 +56,24 @@ class TeamController extends Controller
     {
         $request->validate([
             'name'          => ['required'],
-            'description'   => ['required']
+            'description'   => ['required'],
+            'public'   => ['required'],
         ]);
 
         $team = new Team();
-
         $team->name = $request->name;
         $team->description = $request->description;
-
+        $team->public = $request->public;
         $team->save();
 
-        return response('Successfull creation of team')->json(
-            $team->toArray()
-        );
+        $user = Auth::user();
+        $teamMember = new TeamMember();
+        $teamMember->team_id = $team->id;
+        $teamMember->user_id = $user->id;
+        $teamMember->role_id = config('teamMemberRoles.adminRole');
+        $teamMember->save();
+
+        return response()->json($team);
     }
 
     /**
@@ -47,13 +83,13 @@ class TeamController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $team_id)
     {
         $request->validate([
             'publish' => ['boolean']
         ]);
 
-        $team = Team::find($id);
+        $team = Team::find($team_id);
 
         if (isset($request->name) && $request->name) {
             $team->name = $request->name;
@@ -67,8 +103,8 @@ class TeamController extends Controller
             $team->publish = $request->publish;
         }
 
-        return response('Successful update of team')->json(
-            $team->toArray()
+        return response()->json(
+            $team
         );
     }
 
@@ -78,8 +114,53 @@ class TeamController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($team_id)
     {
-        //
+        Team::destroy($team_id);
+
+        return response()->json([
+            'message' => 'Successful deletion of team'
+        ]);
+    }
+
+    public function createMember(Request $request, $team_id) {
+        $request->validate([
+            'user_id' => ['required']
+        ]);
+
+        $user = User::find($request->user_id);
+
+        $member = new TeamMember();
+        $member->user_id = $user->id;
+        $member->team_id = $team_id;
+        $member->role_id = config('teamMemberRoles.simpleMember');
+        $member->save();
+    }
+
+    public function editMember(Request $request, $team_id, $member_id) {
+        $request->validate([
+            'role_id' => 'required'
+        ]);
+
+        $member = TeamMember::find($member_id)->where('team_id', $team_id);
+
+        if (!$member) {
+            throw new \Exception('Member is not part of this team');
+        }
+
+        $member->role_id = $request->role_id;
+        $member->save();
+
+        return response()->json([
+            'message' => 'Successful editing of member'
+        ]);
+    }
+
+    public function deleteMember($member_id) {
+        TeamMember::destroy($member_id);
+
+        return response()->json([
+            'message' => 'Successful deletion of member'
+        ]);
     }
 }
