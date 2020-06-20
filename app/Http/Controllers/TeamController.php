be8b244c53dc2f8523918ce303925ae268d6ff5c
Ioannis Maliouris <maliouri@csd.auth.gr>
2020-06-20 22:23:41 +0300

    Add member status and retrieve values for team edit

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -19,7 +19,7 @@ class TeamController extends Controller
         $user = Auth::user();
         $teams = Team::whereHas('members', function (Builder $q) use ($user) {
             $q->where('users.id', $user->id);
-        });
+        })->without('members');
 
         return response()->json($teams->get());
     }
@@ -32,18 +32,26 @@ class TeamController extends Controller
         $team = Team::where('id', $team_id);
 
         if ($team->first()->public) {
-            $team = $team->with(['posts', 'members.data']);
+            $team = $team->with(['posts']);
         } else {
             $user = Auth::user();
             $is_member = $team->whereHas('members', function (Builder $q) use ($user) {
                 $q->where('users.id', $user->id);
-            });
+            })->first();
             if ($is_member) {
-                $team = $team->with(['posts', 'members.data']);
+                $team = $team->with(['posts']);
             }
         }
 
         return response()->json($team->first());
+    }
+
+    public function readForEdit($team_id) {
+        $team = Team::where('id', $team_id);
+
+        $teams = $team->with(['posts', 'members.data', 'pendingMembers.data']);
+
+        return response()->json($teams->first());
     }
 
     /**
@@ -113,7 +121,7 @@ class TeamController extends Controller
     public function editMany() {
         $user = Auth::user();
         $teams = Team::whereHas('members', function (Builder $q) use ($user) {
-            $q->where('users.id', $user->id)->where('role_id', config('teamMemberRoles.adminRole'));
+            $q->where('users.id', $user->id)->where('role_id', config('teamMemberRoles.adminRole'))->where('team_members.status', 'like', 'accepted');
         });
 
         return response()->json($teams->get());
@@ -136,12 +144,12 @@ class TeamController extends Controller
 
     public function getMembers($team_id) {
         $team = new Team($team_id);
-        $members = $team->load('members')->get();
+        $members = $team->load('members')->where('team_members.status', 'like', 'accepted')->get();
 
         return response()->json($members);
     }
 
-    private function createMember($user_id, $team_id) {
+    private function createMember($team_id, $user_id) {
         $member = new TeamMember();
         $member->user_id = $user_id;
         $member->team_id = $team_id;
@@ -149,34 +157,47 @@ class TeamController extends Controller
         $member->save();
     }
 
-    public function addMember(Request $request, $team_id) {
-        $request->validate([
-            'user_id' => ['required']
+    public function teamMemberApply($team_id) {
+        $user = Auth::user();
+        $member = TeamMember::where('team_id', $team_id)->where('user_id', $user->id)->first();
+        if ($member) {
+            throw new \Exception('You are already member or you have already applied for this team');
+        }
+
+        $this->createMember($team_id, $user->id);
+
+        return response()->json([
+            'message' => 'Successful application of user for team with id: '.$team_id
         ]);
+    }
 
-        $user = User::find($request->user_id);
+    public function teamMemberAccept(Request $request, $team_id, $user_id) {
+        $member = TeamMember::where('user_id', $user_id)->where('team_id', $team_id)->where('status', 'like', 'pending')->first();
+        if (!$member) {
+            throw new \Exception('User ddin\'t apply for this team or team doesn\'t exist');
+        }
 
-        $this->createMember($user->id, $team_id);
-
+        $member->status = 'accepted';
+        $member->save();
         return response()->json([
             'message' => 'Successful addition of member'
         ]);
     }
 
-    public function addMembers(Request $request, $team_id) {
-        $request->validate([
-            'user_ids' => ['required']
-        ]);
+    // public function addMembers(Request $request, $team_id) {
+    //     $request->validate([
+    //         'user_ids' => ['required']
+    //     ]);
 
-        foreach ($request->user_ids as $user_id) {
-            $user = User::find($user_id);
-            $this->createMember($user->id, $team_id);
-        }
+    //     foreach ($request->user_ids as $user_id) {
+    //         $user = User::find($user_id);
+    //         $this->createMember($user->id, $team_id);
+    //     }
 
-        return response()->json([
-            'message' => 'Successful addition of members'
-        ]);
-    }
+    //     return response()->json([
+    //         'message' => 'Successful addition of members'
+    //     ]);
+    // }
 
     public function editMember(Request $request, $team_id, $member_id) {
         $request->validate([
@@ -197,8 +218,8 @@ class TeamController extends Controller
         ]);
     }
 
-    public function deleteMember($member_id) {
-        TeamMember::destroy($member_id);
+    public function deleteMember($team_id, $user_id) {
+        TeamMember::where('team_id', $team_id)->where('user_id', $user_id)->delete();
 
         return response()->json([
             'message' => 'Successful deletion of member'
